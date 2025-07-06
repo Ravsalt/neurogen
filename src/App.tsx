756e9025eb3d8f0a20db529db3cc8e6fa4d0470c
ravsalt <ravpog10@gmail.com>
2025-07-06 23:47:12 +0800

    feat: Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
